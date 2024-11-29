3099ad2368b5581903890b0a27d802666283f331
Geronimo <info@geowolf.nl>
2024-11-29 20:13:30 +0100

    Fixed home page some more

--- app/page.tsx
+++ app/page.tsx
@@ -10,14 +10,14 @@ export default function Page() {
         {`I’m a lead consultant, developer and all-around tech enthusiast with a passion for
         technology and entrepreneurship. My work so far spans cloud architecture, data
         solutions and building SaaS products from the ground up. I’m all about keeping 
-        things efficient, scalable, and simple—just like thiswebsite.`}
+        things efficient, scalable and simple - just like this website.`}
       </p>
       <p className="mb-4">
         {`This site is a collection of my projects, thoughts and ideas as I navigate
         the exciting (and sometimes chaotic) world of technology and entrepreneurship.
-        It’s also a space where I share what I’ve learned along the way — the good and 
+        It’s also a space where I share what I’ve learned along the way - the good and 
         the bad because let’s face it, mistakes make for the best stories. Thanks for 
-        stopping by — feel free to reach out or dive into the blog!`}
+        stopping by - feel free to reach out or dive into the blog!`}
       </p>
       <div className="my-8">
         <BlogPosts />
