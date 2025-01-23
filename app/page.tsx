e57058f684c4396ca31dc4dd81d5f0df593c60c9
Geronimo <info@geowolf.nl>
2025-01-23 10:48:56 +0100

    Fixed home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         Hi, I'm Geronimo Wolf
       </h1>
       <h2 className="mb-6 font-medium tracking-tight text-gray-500">
-        Sharing Ideas and Insights
+        Tech enthusiast, aspiring entrepreneur, and father.
       </h2>
       
       <div className="prose prose-neutral dark:prose-invert">
@@ -18,15 +18,15 @@ export default function Page() {
         </p>
         <p className="mb-4">
           {`This is not a place for quick thrills or flashy content (as you can 
-          probably tell by the lack of styling). It's a calm corner for thoughtful 
+          probably tell by the lack of styling). It's a calm corner for 
           exploration - whether about tech innovations, lessons learned from building 
           something new, or simple reflections on balancing work and family life.`}
         </p>
 
         <p className="mb-4">
-          {`If you're looking for grounded insights or practical ideas, I hope 
-          you'll find value in what's shared here. Feel free to browse, reflect, 
-          and reach out through LinkedIn if you feel like it.`}
+          {`If you're looking for new insights or practical ideas, I hope 
+          you'll find something valuable. Feel free to reach out through 
+          LinkedIn if you feel like it. (I'll try to get an e-mailaddress up soon).`}
         </p>
 
         <p className="mb-4">
